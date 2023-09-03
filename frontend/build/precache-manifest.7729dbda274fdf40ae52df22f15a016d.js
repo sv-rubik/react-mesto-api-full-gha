6a22fb513f46a8a55cef7a737d8f9b4162f71781
sv-rubik <tosvr@yandex.ru>
2023-09-03 12:14:00 +0300

    new build

---
+++ frontend/build/precache-manifest.7729dbda274fdf40ae52df22f15a016d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1a109e9e945dbcecd97",
+    "url": "./static/css/main.d23fcf8e.chunk.css"
+  },
+  {
+    "revision": "a1a109e9e945dbcecd97",
+    "url": "./static/js/main.80c240cf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "47afc32e6ffb85e82d4f",
+    "url": "./static/js/2.463a01a9.chunk.js"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "./static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "./static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "./static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "./static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "./static/media/plus.bdfb8ebb.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "./static/media/edit-icon.c8252db2.svg"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "./static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "34c4685038e8cf551566cb4ee96e20e5",
+    "url": "./static/media/avatar-edit-icon.34c46850.svg"
+  },
+  {
+    "revision": "45a5fb8f7d30bc622e58a6fca1e99da1",
+    "url": "./static/media/close-icon.45a5fb8f.svg"
+  },
+  {
+    "revision": "933106eaf93ffbb3807048cfeeecbcef",
+    "url": "./static/media/like-icon-active.933106ea.svg"
+  },
+  {
+    "revision": "cb5f16439886c2d2781c07c597fe5b1a",
+    "url": "./static/media/like-icon.cb5f1643.svg"
+  },
+  {
+    "revision": "9c3b03ae735a14dc757b4d25c0ba5fb3",
+    "url": "./static/media/delete-icon.9c3b03ae.svg"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "./static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "223359275f4bedbe62175da6ebac13d5",
+    "url": "./static/media/logo.22335927.svg"
+  },
+  {
+    "revision": "7fc4c149eaeefd2cf76f1e1b8ed62cb4",
+    "url": "./static/media/tickIcon.7fc4c149.svg"
+  },
+  {
+    "revision": "c32299fa3125c41146aa8359b2307523",
+    "url": "./static/media/crossIcon.c32299fa.svg"
+  },
+  {
+    "revision": "4e06a852f1dafa0f58f0d126e83d063e",
+    "url": "./index.html"
+  }
+];
